a74a9523b93bd1517173b43c79506383c8807bf5
Alejandro Aguirre <aguirreee21@gmail.com>
2022-07-27 17:11:59 -0300

    create directive component

--- Lista-Aguirre/src/app/app.module.ts
+++ Lista-Aguirre/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DirectiveComponent } from './components/directive/directive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectiveComponent
   ],
   imports: [
     BrowserModule
